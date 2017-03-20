df65ec50ae6a83de6c557fd0604feb01eacceac9
CupOfTea696 <cupoftea696@gmail.com>
2017-03-20 15:35:48 +0000

    Update tests for Traversable

--- tests/FluentTest.php
+++ tests/FluentTest.php
@@ -6,7 +6,7 @@ class SupportFluentTest extends PHPUnit_Framework_TestCase
 {
     public function testAttributesAreSetByConstructor()
     {
-        $array = ['name' => 'Sven', 'age' => 23];
+        $array = ['name' => 'Sven', 'age' => 24];
         $fluent = new Fluent($array);
         
         $refl = new ReflectionObject($fluent);
@@ -19,8 +19,22 @@ class SupportFluentTest extends PHPUnit_Framework_TestCase
     
     public function testAttributesAreSetByConstructorGivenStdClass()
     {
-        $array = ['name' => 'Sven', 'age' => 23];
+        $array = ['name' => 'Sven', 'age' => 24];
         $fluent = new Fluent((object) $array);
+        
+        $refl = new ReflectionObject($fluent);
+        $attributes = $refl->getProperty('attributes');
+        $attributes->setAccessible(true);
+        
+        $this->assertEquals($array, $attributes->getValue($fluent));
+        $this->assertEquals($array, $fluent->getAttributes());
+    }
+    
+    public function testAttributesAreSetByConstructorGivenArrayable()
+    {
+        $array = ['name' => 'Sven', 'age' => 24];
+        $fluent = new Fluent((object) $array);
+        $fluent = new Fluent($fluent);
         
         $refl = new ReflectionObject($fluent);
         $attributes = $refl->getProperty('attributes');
@@ -32,7 +46,7 @@ class SupportFluentTest extends PHPUnit_Framework_TestCase
     
     public function testAttributesAreSetByConstructorGivenArrayIterator()
     {
-        $array = ['name' => 'Sven', 'age' => 23];
+        $array = ['name' => 'Sven', 'age' => 24];
         $fluent = new Fluent(new FluentArrayIteratorStub($array));
         
         $refl = new ReflectionObject($fluent);
@@ -63,43 +77,55 @@ class SupportFluentTest extends PHPUnit_Framework_TestCase
         $fluent = new Fluent();
         
         $fluent->set('name', 'Sven');
-        $fluent->age = 23;
+        $fluent->age = 24;
         
         $this->assertEquals('Sven', $fluent->get('name'));
-        $this->assertEquals(23, $fluent->age);
+        $this->assertEquals(24, $fluent->age);
         
         $fluent = new Fluent();
         
-        $fluent->set('profile.age', 23);
+        $fluent->set('profile.age', 24);
         $fluent->set('profile.developer', true, false);
         
-        $this->assertEquals(23, $fluent->profile['age']);
+        $this->assertEquals(24, $fluent->profile['age']);
         $this->assertTrue($fluent->get('profile.developer'));
         $this->assertTrue($fluent->get('profile.developer', false));
     }
     
+    public function testFluentIsTraversable()
+    {
+        $array = ['name' => 'Sven', 'age' => 24];
+        $fluent = new Fluent($array);
+        
+        foreach ($fluent as $key => $value) {
+            $iterated[$key] = $value;
+        }
+        
+        $this->assertEquals($array, $iterated);
+    }
+    
     public function testMagicMethodCanBeUsedToGetAttributes()
     {
-        $fluent = new Fluent;
+        $fluent = new Fluent();
         
         $fluent->name = 'Sven';
         
         $this->assertEquals('Sven', $fluent('name'));
         $this->assertNull($fluent('age'));
-        $this->assertEquals(23, $fluent('age', 23));
+        $this->assertEquals(24, $fluent('age', 24));
     }
     
     public function testMagicMethodsCanBeUsedToSetAttributes()
     {
-        $fluent = new Fluent;
+        $fluent = new Fluent();
         
         $fluent->name('Sven');
         $fluent->developer();
-        $fluent->age(23);
+        $fluent->age(24);
         
         $this->assertEquals('Sven', $fluent->name);
         $this->assertTrue($fluent->developer);
-        $this->assertEquals(23, $fluent->age);
+        $this->assertEquals(24, $fluent->age);
         $this->assertInstanceOf(Fluent::class, $fluent->programmer());
     }
     
@@ -129,7 +155,7 @@ class SupportFluentTest extends PHPUnit_Framework_TestCase
     
     public function testToArrayReturnsAttribute()
     {
-        $array = ['name' => 'Sven', 'age' => 23];
+        $array = ['name' => 'Sven', 'age' => 24];
         $fluent = new Fluent($array);
         
         $this->assertEquals($array, $fluent->toArray());
